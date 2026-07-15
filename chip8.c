1f2614e0f06e060a3e66384f727053aa8367996a
jham06 <jhamthebeast227@gmail.com>
2026-07-15 00:01:19 -0600

    less comments, more compact

---
+++ chip8.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "chip8.h" // forgor to include this lol
+
+// Need to include the necessary helper functions. 
+
+
+
+void chip8_initial () { // zero out everything (RAM, registers, display, stack, stack, keypad timers.) 
+// Also set PC to 0x200 and load font data into RAM at 0x050. Just assign to hexadecimal
+    
+}
+
+void chip8_load () { // This should open the ROM file and load it into Ram starting at 0x200
+
+// Have to rememeber, the bytes for the chars are stored from 0x050 to 0x09f or wtv. 
+
+}
+
+
+int main (){
+
+    // I want to use the fetch/decode/execute code here, instead of having a separate function. 
+}
--- chip8.h
+++ chip8.h
@@ -2,19 +2,17 @@
 #define CHIP8_H // I am now back in C, in order to avoid repeated defines of struct, need to implement the ifndef and define. 
 
 #include <stdint.h> // Need to include this in order to work with numbers in C
-#include <stdlib.h>
-
 
 // Need to initialize the components of the emulator
 typedef struct {
-    uint8_t ram[4096]; // Ram that stores 4096 bytes
-    uint8_t registers[16]; // it requires 16 general pupose registers from v0-vf
-    uint8_t display[64*32]; // Ill make this a 1d array for simplciity, it is a display buffer 
+    uint8_t ram[4096]; // Stores 4096 bytes
+    uint8_t registers[16]; // store 16 registers from v0-vf
+    uint8_t display[64*32]; // 1d array for simplciity, tis a display buffer 
 
 
-    uint16_t p_counter;
-    uint16_t index_reg; // specfically, these are 16 bits where 12 are used to access the 4096 bytes in ram
-    uint16_t stack[16]; // because the chip8 had limited space, i can only have 16 levels
+    uint16_t pc; 
+    uint16_t idx; // specfically, these are 16 bits where 12 are used to access the 4096 bytes. Both pc and index
+    uint16_t stack[16]; // because the chip8 had limited space, can only have 16 levels
     uint16_t sp; // apprently i cant declare ptr in struct :(
 
 
