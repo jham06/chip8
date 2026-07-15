f6d699898a5f2000160315e92674ae62b4050a71
Justin Ham <hham1@ualberta.ca>
2026-07-14 23:23:09 -0600

    finished initializing!

--- chip8.h
+++ chip8.h
@@ -4,6 +4,8 @@
 #include <stdint.h> // Need to include this in order to work with numbers in C
 #include <stdlib.h>
 
+
+// Need to initialize the components of the emulator
 typedef struct {
     uint8_t ram[4096]; // Ram that stores 4096 bytes
     uint8_t registers[16]; // it requires 16 general pupose registers from v0-vf
@@ -13,14 +15,12 @@ typedef struct {
     uint16_t p_counter;
     uint16_t index_reg; // specfically, these are 16 bits where 12 are used to access the 4096 bytes in ram
     uint16_t stack[16]; // because the chip8 had limited space, i can only have 16 levels
-    uint16_t *sp = stack;
+    uint16_t sp; // apprently i cant declare ptr in struct :(
 
 
     uint8_t keypad[16]; // checks to see if any of the 16 keys is pressed or not
     uint8_t delay_timer;
     uint8_t sound_timer; // these are both 8 bits
-
-
 
 } Chip8;
 
